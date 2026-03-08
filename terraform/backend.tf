86e8404115223305684f95145213e710ee1cf39d
Gavin Dack <rathoelvis76@gmail.com>
2026-03-08 14:29:14 +0000

    add terraform remote state

---
+++ terraform/backend.tf
@@ -0,0 +1,8 @@
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "rg-terraform-state"
+    storage_account_name = "owltekterraformstate"
+    container_name       = "tfstate"
+    key                  = "management-groups.tfstate"
+  }
+}
